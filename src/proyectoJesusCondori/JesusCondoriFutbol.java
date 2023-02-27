4921d3b1f6fafd25301e628ac447178ad4e5bb5a
Vespertino <Vespertino@C04PC18.informatica.comercio>
2023-02-27 16:36:14 +0100

    Primer commit

---
+++ src/proyectoJesusCondori/JesusCondoriFutbol.java
@@ -0,0 +1,13 @@
+package proyectoJesusCondori;
+
+public class JesusCondoriFutbol {
+	public String equipo;
+	public String nombreHobby;
+	public int numeroFavorito;
+	
+	public JesusCondoriFutbol(String equipo, String nombreHobby, int numeroFavorito) {
+		this.equipo=equipo;
+		this.nombreHobby=nombreHobby;
+		this.numeroFavorito=numeroFavorito;
+	}
+}
